86b755955aaf3a09229907e9ca2ee144c063b1b8
Thang Phi <37077554+thangpmedia@users.noreply.github.com>
2021-03-06 13:46:14 +0700

    Update install_centos8.sh

--- scripts/install_centos8.sh
+++ scripts/install_centos8.sh
@@ -47,12 +47,13 @@ install_3proxy() {
 gen_3proxy() {
     cat <<EOF
 daemon
-maxconn 2000
+maxconn 500
 nserver 1.1.1.1
 nserver 8.8.4.4
 nserver 2001:4860:4860::8888
 nserver 2001:4860:4860::8844
 nscache 65536
+nscache6 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
